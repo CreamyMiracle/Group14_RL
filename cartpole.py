28f4566f3507e1804f8216750a72ea27bcf81c43
Nikita Melentyev <nikita.melentyev@gmail.com>
2020-11-28 21:12:46 +0200

    Initialized q_learing branch and added solution. For plott look rows 359-365

--- cartpole.py
+++ cartpole.py
@@ -1,3 +1,16 @@
+import random
+import gym
+import numpy as np
+import os
+import pickle
+import matplotlib.pyplot as plt
+from collections import deque
+from keras.models import Sequential
+from keras.models import load_model
+from keras.layers import Dense
+from keras.optimizers import Adam
+
+
 """
 Classic cart-pole system implemented by Rich Sutton et al.
 Copied from http://incompleteideas.net/sutton/book/code/pole.c
@@ -5,10 +18,8 @@ permalink: https://perma.cc/C9ZM-652R
 """
 
 import math
-import gym
 from gym import spaces, logger
 from gym.utils import seeding
-import numpy as np
 
 
 class CartPoleEnv(gym.Env):
@@ -54,7 +65,7 @@ class CartPoleEnv(gym.Env):
     metadata = {
         'render.modes': ['human', 'rgb_array'],
         'video.frames_per_second': 50
-    }    
+    }
 
     def __init__(self):
         self.gravity = 9.8
@@ -70,6 +81,11 @@ class CartPoleEnv(gym.Env):
         # Angle at which to fail the episode
         self.theta_threshold_radians = 12 * 2 * math.pi / 360
         self.x_threshold = 2.4
+
+        # Instance of dropping the pole
+        self.dropping = False
+        self.drop_threshold = np.pi / 2
+        self.least_theta = 100.
 
         # Angle limit set to 2 * theta_threshold_radians so failing observation
         # is still within bounds.
@@ -87,9 +103,6 @@ class CartPoleEnv(gym.Env):
         self.state = None
 
         self.steps_beyond_done = None
-        
-        # Pole starting position 0 = up, 1 = down
-        self.starting_position = 0 
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
@@ -98,7 +111,7 @@ class CartPoleEnv(gym.Env):
     def step(self, action):
         err_msg = "%r (%s) invalid" % (action, type(action))
         assert self.action_space.contains(action), err_msg
-        
+
         x, x_dot, theta, theta_dot = self.state
         force = self.force_mag if action == 1 else -self.force_mag
         costheta = math.cos(theta)
@@ -123,37 +136,55 @@ class CartPoleEnv(gym.Env):
 
         self.state = (x, x_dot, theta, theta_dot)
 
-        done = bool(
+        fail = False
+
+        theta = self.normalize_angle(theta)
+
+        # handling of drop factor
+        if abs(theta) > self.least_theta:
+            if self.dropping:
+                if abs(theta) > self.drop_threshold:
+                    fail = True
+            else:
+                if self.least_theta < np.pi / 3:
+                    self.dropping = True
+
+                # if cart moves out of the focus
+        out = bool(
             x < -self.x_threshold
             or x > self.x_threshold
-            or theta < -self.theta_threshold_radians
-            or theta > self.theta_threshold_radians
         )
 
-        if not done:
-            reward = 1.0
-        elif self.steps_beyond_done is None:
-            # Pole just fell!
-            self.steps_beyond_done = 0
-            reward = 1.0
-        else:
-            if self.steps_beyond_done == 0:
-                logger.warn(
-                    "You are calling 'step()' even though this "
-                    "environment has already returned done = True. You "
-                    "should always call 'reset()' once you receive 'done = "
-                    "True' -- any further steps are undefined behavior."
-                )
-            self.steps_beyond_done += 1
+        reward = 0.0
+        if out:
             reward = 0.0
+        elif fail:
+            reward = 0.0
+        else:
 
-        return np.array(self.state), reward, done, {}
+            reward = abs(np.pi - abs(theta)) ** 2 - 0.01 * (theta_dot ** 2) - 0.1 * (x_dot ** 2)
+
+            if abs(theta) == 0:
+                reward *= 2.
+            if abs(theta) < np.pi and abs(x) < 0.25:
+                reward *= 2.
+
+            # saving essential parameters
+            self.least_theta = abs(theta) if self.least_theta > abs(theta) else self.least_theta
+
+        return np.array(self.state), reward, out or fail, {}
+
+
+    def normalize_angle(self, x):
+        return (((x + np.pi) % (2 * np.pi)) - np.pi)
 
     def reset(self):
-        self.state = self.np_random.uniform(low=-0.05, high=0.05, size=(4,))
-        if (self.starting_position == 1): # down
-            self.state[2] = self.state[2] + np.pi/2
+        #self.state = self.np_random.uniform(low=-0.02, high=0.02, size=(4,))
+        self.state = np.zeros((4,))
+        self.state[2] += np.pi
         self.steps_beyond_done = None
+        self.dropping = False
+        self.least_theta = 100.0
         return np.array(self.state)
 
     def render(self, mode='human'):
@@ -214,16 +245,132 @@ class CartPoleEnv(gym.Env):
         if self.viewer:
             self.viewer.close()
             self.viewer = None
-            
-            
-Env = CartPoleEnv()
-Env.reset()
-for i in range(1000):
-    Env.render()
-    action = 1
-    if Env.state[2] < 0 and Env.state[3] < 0 and Env.state[1] > 0:
-        action = 0
-    Env.step(action)
-Env.close()    
-       
-        
+
+ENV_NAME = "CartPole-v1"
+
+GAMMA = 0.95
+LEARNING_RATE = 0.0001
+
+MEMORY_SIZE = 1000000
+BATCH_SIZE = 20
+
+EXPLORATION_MAX = 1.0
+EXPLORATION_MIN = 0.001
+EXPLORATION_DECAY = 0.9999
+
+
+class DQNSolver:
+
+    def __init__(self, observation_space, action_space):
+        self.exploration_rate = EXPLORATION_MAX
+
+        self.action_space = action_space
+        self.memory = deque(maxlen=MEMORY_SIZE)
+
+        path = "model1000"
+
+        if os.path.isfile(path):
+            self.model = load_model(path)
+            self.memory = pickle.load(open(path + ".mem", "rb"))
+            self.exploration_rate = EXPLORATION_MIN
+            print("Model loaded.")
+        else:
+            self.model = Sequential()
+            self.model.add(Dense(64, input_shape=(observation_space,), activation="relu"))
+            self.model.add(Dense(64, activation="relu"))
+            self.model.add(Dense(32, activation="relu"))
+            self.model.add(Dense(self.action_space, activation="linear"))
+            self.model.compile(loss="mse", optimizer=Adam(lr=LEARNING_RATE))
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() < self.exploration_rate:
+            return random.randrange(self.action_space)
+        q_values = self.model.predict(state)
+        return np.argmax(q_values[0])
+
+    def experience_replay(self):
+        if len(self.memory) < BATCH_SIZE:
+            return
+        batch = random.sample(self.memory, BATCH_SIZE)
+        for state, action, reward, state_next, terminal in batch:
+            q_update = reward
+            if not terminal:
+                q_update = (reward + GAMMA * np.amax(self.model.predict(state_next)[0]))
+            q_values = self.model.predict(state)
+            q_values[0][action] = q_update
+            self.model.fit(state, q_values, verbose=0)
+        self.exploration_rate *= EXPLORATION_DECAY
+        self.exploration_rate = max(EXPLORATION_MIN, self.exploration_rate)
+
+    def save(self, name):
+        self.model.save(name)
+        pickle.dump(self.memory, open(name + ".mem", "wb"))
+        print("Model saved!")
+
+
+def cartpole():
+    env = CartPoleEnv()
+    observation_space = env.observation_space.shape[0]
+    action_space = env.action_space.n
+    dqn_solver = DQNSolver(observation_space, action_space)
+
+    run = 1000
+    SAVE_THRESHOLD = 100
+    run_after_save = 0
+    MAX_TIME = 300
+    POINTNTS_TO_COMPLETE = 2500
+    complete_counter = 0
+    scores = []
+    epochs = []
+
+    while True:
+        if run_after_save == SAVE_THRESHOLD:
+            dqn_solver.model.save("model" + str(run))
+            pickle.dump(dqn_solver.memory, open("model" + str(run) + ".mem", "wb"))
+            print("Checkpoint reached!")
+            run_after_save = 0
+        run += 1
+        run_after_save += 1
+        state = env.reset()
+        state = np.reshape(state, [1, observation_space])
+        step = 0
+        time = 0
+        while time < MAX_TIME:
+        #while True:
+            time += 1
+            env.render()
+            action = dqn_solver.act(state)
+            state_next, reward, terminal, info = env.step(action)
+            step += reward
+            state_next = np.reshape(state_next, [1, observation_space])
+            dqn_solver.remember(state, action, reward, state_next, terminal)
+            state = state_next
+            if terminal or time == MAX_TIME:
+                print("Run: " + str(run) + ", exploration: " + str(dqn_solver.exploration_rate) + ", score: " + str(int(step)))
+                break
+            dqn_solver.experience_replay()
+
+        scores.append(step)
+        epochs.append(run)
+
+        plt.clf()
+        plt.plot(epochs, scores)
+        plt.title("Rewards over time (Q-learning)")
+        plt.xlabel("Epoch")
+        plt.ylabel("Reward")
+        plt.draw()
+        plt.show(block=False)
+
+        if step > POINTNTS_TO_COMPLETE:
+            complete_counter += 1
+            if complete_counter == 3:
+                dqn_solver.save("final_model")
+        else:
+            complete_counter = 0
+
+
+if __name__ == "__main__":
+    cartpole()
